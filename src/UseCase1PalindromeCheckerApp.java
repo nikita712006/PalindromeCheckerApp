0ccc3009bd0339b46f0b075315878a96844f8228
nikita712006 <nikita.yadav17770@gmail.com>
2026-02-26 15:43:45 +0530

    UC3: Palindrome using loop

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -27,4 +27,16 @@ public class UseCase1PalindromeCheckerApp {
     } else {
         System.out.println(input + " is NOT a Palindrome");
     }
+    //USE CASE 3
+    String input = "madam";
+    String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+        reversed += input.charAt(i);
+    }
+
+        if (input.equals(reversed)) {
+        System.out.println(input + " is a palindrome");
+    } else {
+        System.out.println(input + " is not a palindrome");
 }
